655f02bb523f9015d5c9c5b8c179b097c999ea49
anilkaragoz <anil.karagoz@d-teknoloji.com.tr>
2024-07-27 05:47:24 +0300

    feat(movie-app): add images and create main pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import "./App.css";
-import Button from "@mui/material/Button";
+import { Route, Routes } from "react-router-dom";
+import MoviesById from "@/pages/MoviesById";
+import Movies from "@/pages/Movies";
 
 function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <Routes>
+      <Route path="/" element={<Movies />} />
+      <Route path="/movies" element={<MoviesById />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "@/styles/style.css";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
